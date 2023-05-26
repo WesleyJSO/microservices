6e260327319d686fe34a55f62f3ce1a1ba353446
WesleyJSO <josewso55@gmail.com>
2023-05-26 14:55:01 -0300

    release working version 1

--- product-service/src/main/java/com/ms/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/ms/productservice/ProductServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
